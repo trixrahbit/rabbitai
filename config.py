449644ac38308334371e3d5cffed06b9e7aa5297
trixrahbit <aarin.m.bailey@gmail.com>
2024-11-04 19:53:17 -0600

    Test Update

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ settings = Settings()
 client = AzureOpenAI(
     azure_endpoint="https://webit.openai.azure.com",
     api_key="91b76cbcc9da4055bd966a0809476c04",
-    api_version="2024-05-01-preview"
+    api_version="2023-05-15"
 )
 
 
